74b6cd7d8675496f71e566687a57645f8aaa71c3
zjshermanburke <93295056+zjshermanburke@users.noreply.github.com>
2025-05-02 14:04:48 -0400

    Update DataProcessing.py

--- recommendermodel/src/DataProcessing.py
+++ recommendermodel/src/DataProcessing.py
@@ -1,3 +1,4 @@
+import os
 import gzip
 import shutil
 import requests
@@ -46,21 +47,39 @@ def filter_movie_dataframe(df:pl.DataFrame):
 
   return new_df
 
-def lookup_people(director_df:pl.DataFrame, lookup_df:pl.DataFrame):
+def lookup_people(crew_df:pl.DataFrame, lookup_df:pl.DataFrame, save_file:bool=False):
   """
    Performs a lookup of director and writers using unique identifiers provided in director_df using lookup_df
    Args:
     director_df (pl.DataFrame): Director DataFrame containing director and writer information
     lookup_df (pl.DataFrame): DataFrame containing lookup information for directors and writers
+   Returns:
+    pl.DataFrame: DataFrame of writers and directors names with associated movie unique IDs
   """
 
   # Create a dictionary of unique identifiers and name of person
   lookup_dict = dict(zip(lookup_df['nconst'], lookup_df['primaryName']))
-  director
-
-
-
-def combine_dataframe(movie_df:pl.DataFrame, ratings_df:pl.DataFrame, director_df:pl.DataFrame, lookup_df: pl.DataFrame):
+  # Creating a list of crew unique IDs from the comma separated string
+  crew_df = crew_df.with_columns(
+    pl.col("writers").str.split(",").alias("writersList"))
+  # Mapping crew names to crew unique IDs
+  crew_df = crew_df.with_columns(
+    pl.col("writersList").list.eval(
+        pl.element().map_elements(lambda x: lookup_dict.get(x, "\\N"), return_dtype=pl.Utf8)
+    ).alias("writerNames")
+  )
+  
+  if (save_file == True):
+    # If we want to save to a csv file, we need to convert the writerNames list to a string
+    crew_df = crew_df.with_columns(
+    pl.col("writerNames").map_elements(lambda x: ",".join(map(str, x)), return_dtype=pl.Utf8).alias("writers")
+    )
+    # crew_df_save = crew_df.drop("writerNames")
+    crew_df.drop("writerNames").write_csv("crew_df_filtered.csv")
+  
+  return crew_df
+  
+def combine_dataframe(movie_df:pl.DataFrame, ratings_df:pl.DataFrame, crew_df:pl.DataFrame):
   """
   Combines DataFrames into a single DataFrame using unique movie identifier
   Args:
@@ -72,11 +91,11 @@ def combine_dataframe(movie_df:pl.DataFrame, ratings_df:pl.DataFrame, director_d
   """
 
   df = movie_df.join(ratings_df, on='tconst')
-  df = df.join(director_df, on='tconst')
+  df = df.join(crew_df, on='tconst')
 
   return df
 
-def data_preprocessing(title_basics_url:str, ratings_basics_url:str, directors_url, lookup_url):
+def data_preprocessing(title_basics_url:str, ratings_basics_url:str, crew_url, lookup_url):
     """
     Method to perform data collection, cleaning, and transformation
     Args:
@@ -86,16 +105,24 @@ def data_preprocessing(title_basics_url:str, ratings_basics_url:str, directors_u
       pl.DataFrame: Prepared Polars DataFrame containing movie and rating information
     """
     # Creating Polars DataFrames
-    movie_df = create_dataframe(movie_url)
-    ratings_df = create_dataframe(ratings_url)
-    director_df = create_dataframe(directors_url)
-    lookup_df = create_dataframe(lookup_url)
+    movie_df = create_dataframe(title_basics_url)
+    ratings_df = create_dataframe(ratings_basics_url)
 
     # Filter movie DataFrame
     movie_df = filter_movie_dataframe(movie_df)
 
-    # Combine DataFrames into a single DataFrame for lookup and training
-    combined_df = combine_dataframe(movie_df, ratings_df, director_df, lookup_df)
+    if(os.path.exists("sample_data/crew_df_filtered.csv")):
+      # If crew has already been mapped, load file
+      crew_df = pl.read_csv("sample_data/crew_df_filtered.csv") # Load Data
+      print("crew_df 1: ", crew_df.head())
+    else:
+      # Else map director and writer names
+      crew_df = create_dataframe(crew_url)
+      lookup_df = create_dataframe(lookup_url)
+      crew_df = lookup_people(crew_df, lookup_df)
 
+    # Combine DataFrames into a single DataFrame for lookup and training 
+    combined_df = combine_dataframe(movie_df, ratings_df, crew_df)
+    return combined_df
 
     return combined_df
