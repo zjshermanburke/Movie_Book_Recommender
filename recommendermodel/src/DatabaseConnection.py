e7dbd7d6a32c0b2f6545e0e81d3e9407d351632b
Zachary Sherman-Burke <zasherma@zasherma-thinkpadp16vgen1.rht.csb>
2026-06-06 21:43:39 -0400

    Fix Python DatabaseConnection bugs and update CI trigger

--- recommendermodel/src/DatabaseConnection.py
+++ recommendermodel/src/DatabaseConnection.py
@@ -1,4 +1,5 @@
 import psycopg2
+from psycopg2 import sql
 import pandas as pd
 
 class DatabaseConnection:
@@ -11,7 +12,7 @@ class DatabaseConnection:
         password (str): Database login password
         table (str): Table to access
         host (str): Database host
-        port (str): Database port
+        port (int): Database port
     """
     def __init__(self, database:str, user:str, password:str, table:str, host:str = "localhost", port:int = 5432):
         """
@@ -23,27 +24,47 @@ class DatabaseConnection:
         self.table = table
         self.host = host
         self.port = port
-        self.cursor = self.set_connection(database, user, password, host, port)
+        self.cursor = self.set_connection()
 
-    def set_connection(self):
+    def set_connection(self) -> psycopg2.extensions.cursor:
         """
         Sets connection to specified database database
 
         Returns:
             Cursor object
         """
-        connection = psycopg2.connect(database=self.database, user=self.user, password=self.password, host=self.host, port=self.port)
-        cursor = connection.cursor()
-        return cursor
+        self.connection = psycopg2.connect(
+            database=self.database, 
+            user=self.user, 
+            password=self.password, 
+            host=self.host, 
+            port=self.port)
+        return self.connection.cursor()
 
-    def get_data(self):
+    def disconnect(self) -> None:
+        """
+        Disconnects from the database
+        """
+        self.cursor.close()
+        self.connection.close()     
+
+    def query(self, query:str, params:tuple = None) -> list[tuple]:
+        """
+        Executes a query on the database
+        """
+        self.cursor.execute(query, params)
+        return self.cursor.fetchall()
+
+    def get_data(self) -> pd.DataFrame:
         """
         Loads data from database and prepares it
 
         Returns:
             Pandas DataFrame
         """
-        self.cursor.execute("SELECT * from " + self.table + ";")
+        self.cursor.execute(
+            sql.SQL("SELECT * FROM {}").format(sql.Identifier(self.table))
+        )
         record = self.cursor.fetchall()
         column_names = [desc[0] for desc in self.cursor.description]
         return pd.DataFrame(record, columns=column_names)
