681a42acc30c1273a914b2a54a114c8048d670d4
Zachary Sherman-Burke <zasherma@zasherma-thinkpadp16vgen1.rht.csb>
2026-06-06 21:07:29 -0400

    Remove build directory from tracking and update .gitignore

--- librarybuilder/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/zasherma/Dev/ZachsCode/Movie_Book_Recommender/librarybuilder
-# Build directory: /home/zasherma/Dev/ZachsCode/Movie_Book_Recommender/librarybuilder/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(AllTests "/home/zasherma/Dev/ZachsCode/Movie_Book_Recommender/librarybuilder/build/tests")
-set_tests_properties(AllTests PROPERTIES  _BACKTRACE_TRIPLES "/home/zasherma/Dev/ZachsCode/Movie_Book_Recommender/librarybuilder/CMakeLists.txt;49;add_test;/home/zasherma/Dev/ZachsCode/Movie_Book_Recommender/librarybuilder/CMakeLists.txt;0;")
